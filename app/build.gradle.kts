1d969c4154c296dfb0a2fab32452bf298e7e4ebf
SoFranci <trevisanfrancesco022@gmail.com>
2023-10-16 14:43:25 +0200

    restarted working new base support android 24+

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,16 +1,15 @@
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.android")
 }
 
 android {
     namespace = "me.cranci.waifu2d"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "me.cranci.waifu2d"
-        minSdk = 21
-        targetSdk = 33
+        minSdk = 24
+        targetSdk = 31
         versionCode = 1
         versionName = "1.0"
 
@@ -30,14 +29,10 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
 }
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     testImplementation("junit:junit:4.13.2")
